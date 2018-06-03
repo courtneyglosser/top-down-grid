566c0b4f902be5805f4a500d84041d2d1bd39a56
Courtney Glosser <courtneyglosser@gmail.com>
2018-06-02 22:37:37 -0400

    Adding eslint and applying lint rules to src/app.js.  Also, adding a basic canvas

--- src/app.js
+++ src/app.js
@@ -1,35 +1,43 @@
-import "./stylesheets/main.css";
+import { remote } from 'electron';
+import env from 'env';
+import jetpack from 'fs-jetpack';
+
+import './stylesheets/main.css';
 
 // Small helpers you might want to keep
-import "./helpers/context_menu.js";
-import "./helpers/external_links.js";
+import './helpers/context_menu';
+import './helpers/external_links';
 
 // ----------------------------------------------------------------------------
 // Everything below is just to show you how it works. You can delete all of it.
 // ----------------------------------------------------------------------------
 
-import { remote } from "electron";
-import jetpack from "fs-jetpack";
-import { greet } from "./hello_world/hello_world";
-import env from "env";
+import { greet } from './hello_world/hello_world';
 
-const app = remote.app;
+const { app } = remote;
 const appDir = jetpack.cwd(app.getAppPath());
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // files from disk like it's node.js! Welcome to Electron world :)
-const manifest = appDir.read("package.json", "json");
+const manifest = appDir.read('package.json', 'json');
 
 const osMap = {
-  win32: "Windows",
-  darwin: "macOS",
-  linux: "Linux"
+  win32: 'Windows',
+  darwin: 'macOS',
+  linux: 'Linux'
 };
 
-document.querySelector("#app").style.display = "block";
-document.querySelector("#greet").innerHTML = greet();
-document.querySelector("#os").innerHTML = osMap[process.platform];
-document.querySelector("#author").innerHTML = manifest.author;
-document.querySelector("#env").innerHTML = env.name;
-document.querySelector("#electron-version").innerHTML =
+document.querySelector('#app').style.display = 'block';
+document.querySelector('#greet').innerHTML = greet();
+document.querySelector('#os').innerHTML = osMap[process.platform];
+document.querySelector('#author').innerHTML = manifest.author;
+document.querySelector('#env').innerHTML = env.name;
+document.querySelector('#electron-version').innerHTML =
   process.versions.electron;
+
+
+const gameCanvas = document.querySelector('#game');
+
+const ctx = gameCanvas.getContext('2d');
+
+ctx.fillRect(0, 0, 100, 100);
